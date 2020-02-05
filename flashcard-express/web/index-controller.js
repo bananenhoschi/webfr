afe6e511478184584bb7c992cce9430dcc468212
Carlo Eggenschwiler <carlo.eggenschwiler@students.fhnw.ch>
2020-02-05 20:30:09 +0100

    Web Frameworks HS19

--- flashcard-express/web/index-controller.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-const log4js = require('log4js');
-const logger = log4js.getLogger('controller');
-
-exports.index = (req, res) => {
-    logger.debug('index called')
-    res.redirect(req.baseUrl + "/questionnaires")
-}
